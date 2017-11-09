1a9dbb87b4fc6f9bebbd2b8ffeed35446296fd7e
Faundo Barrionuevo <facubarrionuevoo@gmail.com>
2017-11-08 23:56:03 -0300

    terceto, tercetoetiqueta

--- src/CodigoIntermedio/TercetoEtiqueta.java
+++ src/CodigoIntermedio/TercetoEtiqueta.java
@@ -5,4 +5,8 @@ public class TercetoEtiqueta extends Terceto {
     public TercetoEtiqueta(TercetoSimple izq, TercetoSimple medio, TercetoSimple der, int numeroTerceto) {
         super(izq, medio, der, numeroTerceto);
     }
+
+    public String getAsembler(){
+        return "label"+numeroTerceto+":"+"\n";
+    }
 }
